73314688b417362aec91d4d98dca281b83616bcd
Banseok Lee <105290614+levitss@users.noreply.github.com>
2023-04-02 20:51:12 +0900

    [Bronze II] Title: 대표값2, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -1,33 +1,85 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.IntSummaryStatistics;
-import java.util.List;
+import java.io.*;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> list = new ArrayList<>();
-        int sum=0;
-        int a;
-//  평균과 중앙값을 구하기.
-        //중앙값은 수를 크기 순서대로 늘어놓았을 때 가장 중앙에 있는 것
-        for (int i = 0; i < 5; i++) {
-            a = Integer.parseInt(br.readLine());
-            list.add(a);
-            sum+=a;
+//    public static void swap(int[] arr, int idx1, int idx2) {
+//        int temp = arr[idx1];
+//        arr[idx1] = arr[idx2];
+//        arr[idx2] = temp;
+//    }
+
+    public static int[] tempArr;
+
+    public static void sort(int arr[]) {
+        tempArr = new int[arr.length];
+        sort(arr,0, arr.length - 1);
+        tempArr=null;
+    }
+
+    public static void sort(int[] arr, int left, int right) {
+       if(left==right){
+           return;
+       }
+
+            int mid = (right + left) / 2;
+            sort(arr, left, mid);
+            sort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+
+    }
+    public static int sum=0;
+    public static void merge(int arr[], int left, int mid, int right) {
+        int p1=left;
+        int p2=mid+1;
+        int idx = left; //채워넣을 배열의 인덱스
+        while (p1 <= mid && p2 <= right) {
+            if (arr[p1] <= arr[p2]) {//p1 p2 비교해서 작은거 tempArr에 넣기
+                tempArr[idx] = arr[p1];
+                idx++;
+                p1++;
+            } else {
+                tempArr[idx] = arr[p2];
+                idx++;
+                p2++;
+            }
         }
-        Collections.sort(list);
+        if (p1 > mid) {//남은 것들 ..이 있나?
+            while (p2 <= right) {
+                tempArr[idx] = arr[p2];
+                idx++;
+                p2++;
+            }
+
+        } else {
+            while (p1 <= mid) {
+                tempArr[idx] = arr[p1];
+                idx++;
+                p1++;
+            }
+        }
+        for (int i = left; i <= right; i++) {
+            arr[i] = tempArr[i];
+        }
 
 
-        System.out.println(sum/list.size());
-        System.out.println(list.get(2));
 
 
     }
+    public static void main(String[] args) throws IOException {
+
+        //평균과 중앙값 구하기
+        //
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[5];
+        int input;
+        for (int i = 0; i < 5; i++) {
+            input = Integer.parseInt(br.readLine());
+            arr[i] = input;
+            sum += input;
+        }
+        sort(arr);
+        System.out.println(sum/5);
+        System.out.println(arr[2]);
+
+    }
 }
-
-
