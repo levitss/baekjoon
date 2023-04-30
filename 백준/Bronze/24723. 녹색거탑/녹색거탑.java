394e21b6deecae0760c0dc3c5ced54e3850fa362
Banseok Lee <105290614+levitss@users.noreply.github.com>
2023-04-30 17:59:31 +0900

    [Bronze IV] Title: 녹색거탑, Time: 120 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double i =  Double.parseDouble (br.readLine());
+        System.out.println((int)(Math.pow(2, i)));
+
+
+
+        //각각 2군데로
+
+    }
+}
