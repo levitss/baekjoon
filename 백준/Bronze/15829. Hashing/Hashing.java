ff514fa89e4e3d73e66b9e66c901f87c71696ddc
Banseok Lee <105290614+levitss@users.noreply.github.com>
2023-08-16 18:32:31 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuffer sb = new StringBuffer();
+        final long M = 1234567891;
+        //입력은 1~26 알파벳 소문자 한정
+        long sum = 0;
+        String s = br.readLine();
+        for (int i = 0; i < n; i++) {
+            int i1 = s.charAt(i) - 96;
+            sum+=(long)(i1 * Math.pow(31, i));
+        }
+
+        System.out.println(sum);
+
+    }
+
+}
