8214faa3fadb6d9069b6a190e7fed9abf6937d2a
Banseok Lee <105290614+levitss@users.noreply.github.com>
2023-12-08 05:53:58 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        if(a!=b&&b!=c&&c!=a){
+            return a+b+c;
+        }
+        if(a!=b||b!=c||c!=a){
+            return (a+b+c)*(int)(Math.pow(a,2)+Math.pow(c,2)+Math.pow(b,2));
+        }
+        
+        return (a+b+c)*(int)(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2))*(int)(Math.pow(a,3)+Math.pow(b,3)+Math.pow(c,3));
+    }
+}
