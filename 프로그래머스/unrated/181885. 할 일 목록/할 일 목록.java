79b5c99cf0ef2142eb32e1a24d2c49bc47460feb
Banseok Lee <105290614+levitss@users.noreply.github.com>
2023-12-17 00:20:46 +0900

    [level unrated] Title: 할 일 목록, Time: 0.04 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        String[] answer = {};
+        List<String> list=new ArrayList<>();
+        for (int i = 0; i < finished.length; i++) {
+            if(!finished[i]) list.add(todo_list[i]);
+        }
+        return list.toArray(new String[list.size()]);
+    }
+}
