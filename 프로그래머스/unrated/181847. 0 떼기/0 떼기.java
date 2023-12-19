512752da0d8694852638edd4c22703b67d0689dc
Banseok Lee <105290614+levitss@users.noreply.github.com>
2023-12-20 03:06:34 +0900

    [level unrated] Title: 0 떼기, Time: 0.06 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String n_str) {
+        StringBuffer sb=new StringBuffer();
+        for(int i=0;i<n_str.length();i++){
+            if(n_str.charAt(i)!='0') {
+                for(int j=i;j<n_str.length();j++){
+                    sb.append(n_str.charAt(j));
+                }
+                return sb.toString();
+            }
+        }
+        
+        return sb.toString();
+    }
+}
