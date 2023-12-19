58461bae3dd6cca84b37a1c627351f5f03ef5c23
Banseok Lee <105290614+levitss@users.noreply.github.com>
2023-12-20 03:48:26 +0900

    [level unrated] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        //6조각
+        int i=1;
+        while(true){
+            
+            
+            int num=i*6;
+            if(num%n==0) return i;           
+            i++;
+        
+        }
+        
+        
+    }
+}
